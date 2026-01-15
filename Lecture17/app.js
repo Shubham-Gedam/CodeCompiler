16b8d52628f16fddd1381e1d17b09e1795bc171b
Shubham-Gedam <shubhgedam28@gmail.com>
2026-01-15 11:12:18 +0530

    Splice & Slice Methods

---
+++ Lecture17/app.js
@@ -0,0 +1,12 @@
+let items = ['pen', "pensil", "notebook", "Eraser", "sharpener"]
+
+// items.splice(2,3)
+// items.splice(1,0, 'marker')
+// items.splice(2,2 ,'Scale', 'rular' )
+// console.log(items);
+
+
+const preview = items.slice(1,3)
+console.log(preview);
+console.log(items);
+
