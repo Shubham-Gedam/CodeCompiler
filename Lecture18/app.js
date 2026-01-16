5cb8483df0a97d451cae583d4cc83b3cddc31bf8
Shubham-Gedam <shubhgedam28@gmail.com>
2026-01-16 11:46:57 +0530

    Spread & Rest Operator in JavaScript

--- Lecture18/app.js
+++ Lecture18/app.js
@@ -1,26 +1,26 @@
-// const nums = [1,2,3];
-// const copy = [...nums,4];
-// console.log(copy);
+const nums = [1,2,3];
+const copy = [...nums,4];
+console.log(copy);
 
 
-// const newStudent ={
-//     name: 'rudu'
-// }
-// const copy1 = {...newStudent, 'age':8}
-// console.log(copy1);
+const newStudent ={
+    name: 'rudu'
+}
+const copy1 = {...newStudent, 'age':8}
+console.log(copy1);
 
 
-// const a = [1,2,3]
-// const b = [4,5,6]
+const a = [1,2,3]
+const b = [4,5,6]
 
-// const merge  = [...a,...b]
-// console.log(merge);
+const merge  = [...a,...b]
+console.log(merge);
 
-// function sum(a,b,c){
-//    return a+b+c
-// }
-// const values = [1,2,3]
-// console.log(sum(...values));
+function sum(a,b,c){
+   return a+b+c
+}
+const values = [1,2,3]
+console.log(sum(...values));
 
 
 
@@ -28,11 +28,11 @@
 //rest
 
 
-// function logAll(...items){
-//     console.log(items);
-// }
-// logAll("a","b","c")
+function logAll(...items){
+    console.log(items);
+}
+logAll("a","b","c")
 
 
-// const[first,...rest] = [10,20,30,40];
-// console.log(first,rest);
+const[first,...rest] = [10,20,30,40];
+console.log(first,rest);
