db56c2417fb2622b5bd7cb16482e4b196a935408
Shubham-Gedam <shubhgedam28@gmail.com>
2026-01-16 11:46:29 +0530

    Spread & Rest Operator in JavaScript

---
+++ Lecture18/app.js
@@ -0,0 +1,38 @@
+// const nums = [1,2,3];
+// const copy = [...nums,4];
+// console.log(copy);
+
+
+// const newStudent ={
+//     name: 'rudu'
+// }
+// const copy1 = {...newStudent, 'age':8}
+// console.log(copy1);
+
+
+// const a = [1,2,3]
+// const b = [4,5,6]
+
+// const merge  = [...a,...b]
+// console.log(merge);
+
+// function sum(a,b,c){
+//    return a+b+c
+// }
+// const values = [1,2,3]
+// console.log(sum(...values));
+
+
+
+
+//rest
+
+
+// function logAll(...items){
+//     console.log(items);
+// }
+// logAll("a","b","c")
+
+
+// const[first,...rest] = [10,20,30,40];
+// console.log(first,rest);
