991c6c9da831b03ec2311b6addb07c7706ea25e0
Shubham-Gedam <shubhgedam28@gmail.com>
2026-01-17 10:33:47 +0530

    Ternary Operator & Short-Circuiting

---
+++ Lecture19/app.js
@@ -0,0 +1,11 @@
+//Ternary Operator & Short-Circuiting
+
+
+const age = 25;
+
+const status = age>=18 ? "adult" : "minor";
+console.log(status);
+
+
+const input = "" || "defalt values"
+console.log(input);
