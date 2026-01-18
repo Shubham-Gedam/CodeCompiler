745cda03ec97359fd1e973105a5f059e133ef56e
Shubham-Gedam <shubhgedam28@gmail.com>
2026-01-18 10:17:43 +0530

    Boolean Values in JS

---
+++ Lecture20/app.js
@@ -0,0 +1,21 @@
+// Boolean values in js
+   
+       //Falsy Values
+if(0) console.log("run!!");
+if("") console.log("run!!");
+if(null) console.log("run!!");
+if(undefined) console.log("run!!");
+
+
+    //truthy valuse
+if("hello") console.log("run !!!");
+if(42) console.log("run !!!");
+if([]) console.log("run !!!");
+if({}) console.log("run !!!");
+
+
+
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
