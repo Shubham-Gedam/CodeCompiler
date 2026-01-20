cdd98a9fb29fadbd93aac27c4dc476a16cde0298
Shubham-Gedam <shubhgedam28@gmail.com>
2026-01-20 11:01:01 +0530

    Add examples of inheritance using Object.create and ES6 classes

--- Lecture21/app.js
+++ Lecture21/app.js
@@ -23,7 +23,56 @@ person.prototype.greet =function() {
 }
 const user = new person("shubh");
 console.log(user.greet());
-console.log(user.__proto__);
+console.log(user.__proto__); //view in browser console
+
+
+// using Object.create() for inheritance
+// const userModel = {
+//     sayHi: function (){
+//         return `hello ${this.name}`
+//     }
+// }
+// // // const user2 = Object.create(userModel)
+// const user2 = Object.assign({},userModel) //Shallow copy
+// user2.name = 'shubh'
+// console.log(user2.sayHi());
+
+
+function Animal(name){
+    this.name = name;
+}
+Animal.prototype.walk = function(){
+    return `${this.name} is walking`;
+}
+function Dog(name){
+    this.name = name;
+}
+Dog.prototype = Object.create(Animal.prototype)
+Dog.prototype.bark = function(){
+    return `${this.name} is barking`
+}
+const dog = new Dog("tommy")
+console.log(dog.bark());
+console.log(dog.walk());
 
 
 
+//Es6 
+
+class Animal {
+    constructor(name){
+        this.name = name;
+    }
+    walk(){
+        return `${this.name} is walking`
+    }
+}
+class Dog extends Animal {
+    
+    bark(){
+        return `${this.name} is barking`
+    }
+}
+const dog1 = new Dog("tommy")
+console.log(dog1.bark());
+console.log(dog1.walk());
