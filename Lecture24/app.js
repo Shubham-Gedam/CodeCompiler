a3abc9283b4f50013faceb539c46e0b78c3f8a7f
Shubham-Gedam <shubhgedam28@gmail.com>
2026-01-23 11:04:00 +0530

    promise

--- Lecture24/app.js
+++ Lecture24/app.js
@@ -1,35 +1,42 @@
-// function orderFood(isRestaurantOpnen){
-//     return new Promise((resolve, reject) => {
-//         console.log("placing order...");
-//         setTimeout(() => {
-//             if(isRestaurantOpnen){
-//                 resolve ('Order Recieved')
-//             }else{
-//                 reject ('Restaurant is closed')
-//             }
-//         }, 2000);
-//     })
-// }
+function orderFood(isRestaurantOpnen) {
+  return new Promise((resolve, reject) => {
+    console.log("placing order...");
+    setTimeout(() => {
+      if (isRestaurantOpnen) {
+        resolve("Order Recieved");
+      } else {
+        reject("Restaurant is closed");
+      }
+    }, 2000);
+  });
+}
 
-// orderFood(false).then((res)=>{
-//     console.log(res);
-//     return 'preparing food..'
-// }).then((res)=>{
-//     console.log(res);
-//     return "Out of delivery food"
-// }).then((res)=>{
-//     console.log(res);  
-// }).catch((error)=>{
-//     console.log("error", error);
-// }).finally(()=>{
-//     console.log("thanks for ordering food..");
-    
-// })
-
-fetch('https://jsonplaceholder.typicode.com/todos/1').then((response)=>{
-    return response.json();
-}).then((data)=>{
-    console.log(data);
-}).catch((error)=>{
+orderFood(false)
+  .then((res) => {
+    console.log(res);
+    return "preparing food..";
+  })
+  .then((res) => {
+    console.log(res);
+    return "Out of delivery food";
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
     console.log("error", error);
-})
+  })
+  .finally(() => {
+    console.log("thanks for ordering food..");
+  });
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("error", error);
+  });
