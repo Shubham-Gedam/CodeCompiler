1de7c0eb29e3787382c85dcdf567bf5023380ff9
Shubham-Gedam <shubhgedam28@gmail.com>
2026-01-26 18:31:01 +0530

    assessment

---
+++ assessment1.js
@@ -0,0 +1,150 @@
+// console.log(a);
+
+// var a = 10;
+
+// function a() {}
+
+// console.log(a);
+
+
+
+
+// console.log(typeof x);
+
+// let x = 5;
+
+
+// console.log('A');
+
+// setTimeout(() => console.log('B'), 0);
+
+// Promise.resolve().then(() => console.log('C'));
+
+// console.log('D');
+
+
+
+// setTimeout(() => console.log('T1'), 0);
+
+// for (let i = 0; i < 1e6; i++) {}
+
+// console.log('X');
+
+
+// for (var i = 0; i < 3; i++) {
+
+// setTimeout(() => console.log(i), 0);
+
+// }
+
+
+
+
+// const obj = {
+
+// value: 42,
+
+// a: () => console.log(this.value),
+
+// b: function () { console.log(this.value); }
+
+// };
+
+// obj.a();
+
+// obj.b();
+
+
+
+
+// Guess the Output ??
+
+// function makeCounter() {
+
+// let n = 0;
+
+// return () => ++n;
+
+// }
+
+// const c1 = makeCounter();
+
+// const c2 = makeCounter();
+
+// console.log(c1());
+
+// console.log(c1());
+
+// console.log(c2());
+
+
+
+
+
+// const user = {
+
+// _age: 10,
+
+// get age() { return this._age + 1; },
+
+// set age(v) { this._age = v * 2; }
+
+// };
+
+// user.age = 20;
+
+// console.log(user._age, user.age);
+
+
+
+
+// class X {
+
+// #v = 1;
+
+// getV() { return this.#v; }
+
+// }
+
+// const x = new X();
+
+// console.log('v' in x, x.getV());
+
+
+
+
+// const data = { a: { b: 0 } };
+
+// console.log( data.a?.b ?? 5 || 9 );
+
+
+
+
+
+// const { a = 1 } = { a: undefined };
+
+// console.log(a);
+
+
+
+
+
+
+// const { x = 10, y = 20 } = { x: undefined, y: null };
+
+// console.log(x, y);
+
+
+
+
+
+// const [p, , q] = [1, 2, 3];
+
+// console.log(p, q);
+
+
+
+
+// const data = [" ", "", undefined];
+
+// console.log(data.filter(item => item));
---
+++ assessment2.js
@@ -0,0 +1,163 @@
+// const obj = { a: { b: 1 } };
+
+// const c1 = { ...obj };
+
+// c1.a.b = 2;
+
+// console.log(obj.a.b);
+
+
+
+// const obj = { a: { b: 1 } };
+
+// const c2 = structuredClone(obj);
+
+// c2.a.b = 2;
+
+// console.log(obj.a.b);
+
+
+
+// .
+// Guess the Output ?
+
+// console.log([1,2,3].map(x => x*2).filter(x => x > 3).reduce((a,b) => a+b, 0));
+
+
+
+
+// console.log('a,b,,c'.split(',').join('-'));
+
+
+
+// .
+// Guess the Output ?
+
+// const arr = [5,12,8,130,44];
+
+// console.log( arr.find(x => x > 10), arr.findIndex(x => x > 100) )
+
+
+
+
+// const nums = [1,2,3,4];
+
+// console.log(nums.splice(1, 2, 9), nums, nums.slice(1, 3));
+
+
+
+
+
+// .
+// Guess the Output ?
+
+// function add(y, z) { return this.x + y + z; }
+
+// const obj2 = { x: 10 };
+
+// const f = add.bind(obj2, 5);
+
+// console.log( f(2), add.call({x:1}, 2, 3), add.apply({x:2}, [3,4]) );
+
+
+
+
+
+// .
+// Guess the Output ?
+
+// function A() {}
+
+// A.prototype.g = () => 'G';
+
+// const a = new A();
+
+// console.log(a.g(), Object.getPrototypeOf(a) === A.prototype);
+
+
+
+
+// console.log( Boolean([]), Boolean(''), !!null, !!'0' );
+
+
+
+
+
+
+// const sum = (...args) => args.reduce((a,b) => a+b, 0);
+
+// const a1 = [1,2], a2 = [3,4];
+
+// console.log( sum(...a1, ...a2) );
+
+
+
+
+
+
+// const a = 0, b = 2;
+
+// console.log( a || (b > 1 ? 'yes' : 'no') && 'ok' );
+
+
+
+
+
+// const s = new Set([1,2,2,3]);
+
+// const m = new Map([[1,'a']]);
+
+// m.set({}, 'x');
+
+// console.log(s.size, m.has(1), m.size);
+
+
+
+
+
+
+
+
+
+// Guess the Output ?
+
+// document.body.addEventListener('click', () => console.log('body'));
+
+// const div = document.createElement('div');
+
+// document.body.appendChild(div);
+
+// div.addEventListener('click', () => console.log('div'));
+
+// div.click();
+
+
+
+
+
+
+
+// Promise.resolve(1)
+
+// .then(x => x + 1)
+
+// .then(x => { throw x; })
+
+// .catch(x => x + 1)
+
+// .then(console.log);
+
+
+
+
+// async function f() {
+
+// try { return await Promise.reject('err'); }
+
+// catch (e) { return 'caught'; }
+
+// finally { console.log('finally'); }
+
+// }
+
+// f().then(console.log);
